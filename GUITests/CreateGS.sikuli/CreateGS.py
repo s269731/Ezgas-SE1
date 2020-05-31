465295c557b5fdf1f4fca1232179b17d0ca3e4d3
MATTEO MARINO s260363 <s260363@studenti.polito.it>
2020-05-31 17:23:56 +0200

    feat: added CreateGS, EvaluateRight, EvaluateWrong, SearchByCar, SearchByCarSh, SearchByGasType, UpdateGs tests in GUITests

---
+++ GUITests/CreateGS.sikuli/CreateGS.py
@@ -0,0 +1,12 @@
+#Admin is already logged in
+#Create a gas station from admin panel
+wheel(WHEEL_DOWN, 9)
+type("1590680614590.png", "IP station")
+type("1590680632902.png", "Piazza Sabotino")
+wait("gs_list.png")
+click("item.png")
+click("1590680997694.png")
+click("1590681011169.png")
+#click("1590681028476.png")
+#type(Key.PAGE_DOWN)
+#wait("1590681533349.png")

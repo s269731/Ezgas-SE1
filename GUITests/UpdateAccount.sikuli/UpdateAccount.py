5b26afd83ca964180441418499b549232341cb25
Loredana Finocchiaro <s269731@studenti.polito.it>
2020-06-01 18:50:32 +0200

    feat: add Login, Signup, UpdateAccount, DeleteUser, DeleteGS, UpdateDependability and database file used for GUI and Controller tests in GUITests folder

---
+++ GUITests/UpdateAccount.sikuli/UpdateAccount.py
@@ -0,0 +1,5 @@
+#Luigi is already logged in and now wants to update his password
+click("1590594950897.png")
+type("1590594991253.png", "luigi1985")
+click("1590595023695.png")
+wait("1590594890683.png")

67407810b17050f81d95281df060841da7db68ad
s236076 <s236076@studenti.polito.it>
2020-06-01 15:36:20 +0200

    feat: add CreateAccount_admin, SeearchByProximity, SearchWithCoordinates, SetReport, SetReportOverwritten, UpdateAcountAdmin to GUITests

---
+++ GUITests/UpdateAccount_admin.sikuli/UpdateAccount_admin.py
@@ -0,0 +1,5 @@
+#Admin can update information of any user, here he updates Matteo
+click("1590850892309-1.png")
+click("1590852319998.png")
+type("1590852334889.png", "newPass")
+click("1590852351855.png")
